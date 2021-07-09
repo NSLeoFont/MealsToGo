f1111faccc5437d728bc4e58cd2429bf68b2071c
Leo Font <leofontdev@gmail.com>
2021-07-09 16:40:13 +0200

    Organising code, starting restaurant list screen and info card

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
-import { Searchbar } from "react-native-paper";
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants-screen";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const onChangeSearch = (query) => setSearchQuery(query);
-
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    flex: 0,
-  },
-  list: {
-    backgroundColor: "blue",
-    flex: 1,
-    padding: 16,
-  },
-});
